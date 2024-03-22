d67acee098d234a26fb6c2d168ab81b06a2fbf1e
KalaSubramani <kalasubramani@gmail.com>
2024-03-21 23:29:54 -0400

    coding practice 21Mar24

---
+++ Episode.java
@@ -0,0 +1,20 @@
+package HeadFirst;
+
+class Episode {
+ 
+    int seriesNumber;
+    int episodeNumber;
+   
+    void skipIntro() {
+      System.out.println("Skipping intro...");
+    }
+    
+    void skipToNext() {
+      System.out.println("Loading next episode...");
+    }
+
+    void play()
+    {
+        System.out.println("Playing Episode....");
+    }
+  }
---
+++ EpisodeTestDrive.java
@@ -0,0 +1,11 @@
+package HeadFirst;
+
+class EpisodeTestDrive {
+    public static void main(String[] args) {
+  
+      Episode episode = new Episode();
+      episode.seriesNumber = 4;
+      episode.play();
+      episode.skipIntro();
+    }
+  }
